ec30447a7af9f79e7af038a256ec65f68a2aec43
Jesse Strode <jesse_strode@condenast.com>
2020-02-27 07:13:04 -0500

    Updates

---
+++ precache-manifest.05f541c110097288d99ac971cd2f2df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60196450990f99023d54abe07786eaee",
+    "url": "/congius/index.html"
+  },
+  {
+    "revision": "21cc36654fbbd5c93cb6",
+    "url": "/congius/static/css/main.0dbc0b8e.chunk.css"
+  },
+  {
+    "revision": "93ce8036bf13947b9365",
+    "url": "/congius/static/js/2.c2a1822f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/congius/static/js/2.c2a1822f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21cc36654fbbd5c93cb6",
+    "url": "/congius/static/js/main.a755a6cb.chunk.js"
+  },
+  {
+    "revision": "c2806dfc50ade82e9aef",
+    "url": "/congius/static/js/runtime-main.6be29469.js"
+  },
+  {
+    "revision": "ce6b70f32d54547914531c11936ffaff",
+    "url": "/congius/static/media/background.ce6b70f3.jpg"
+  }
+]);
